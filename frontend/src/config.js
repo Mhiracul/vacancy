d5753c6ca116b7c0a045962a8ac57c94a79ef8d9
Miracle Okeke <mokeke185@gmail.com>
2025-10-16 22:25:25 +0100

    first commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://your-backend-domain.com/api"
+    ? "https://vacancy-oa9h.onrender.com/api"
     : "http://localhost:5000/api";
 
 export default BASE_URL;
